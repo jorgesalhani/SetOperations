c4433aabd067fced07965124c8dbd83fe687f7de
jorgesalhani <jorge.salhani@usp.br>
2022-11-21 03:04:40 -0300

    Running. No leaks

--- Src/item.c
+++ Src/item.c
@@ -3,35 +3,34 @@
 #include "item.h"
 
 struct item_{ 
-        int chave;
-        /*outros componentes*/
+   int chave;
+   /*outros componentes*/
 };
 
 ITEM *item_criar (int chave){
-          ITEM *item;
-          
-          item = (ITEM *) malloc(sizeof(ITEM));
-          
-          if (item != NULL){
-             item->chave = chave;
-             return(item);
-          }
-          return(NULL);
+   ITEM *item;
+   
+   item = (ITEM *) malloc(sizeof(ITEM));
+   
+   if (item != NULL){
+      item->chave = chave;
+      return(item);
+   }
+   return(NULL);
 }
 
 bool item_apagar(ITEM **item){
    if (*item != NULL){
       free (*item);
-      *item = NULL; /*Boa prática!*/
+      *item = NULL;
       return(true);
    }
    return(false);
 }
 
 int item_get_chave(ITEM *item){
-    if (item != NULL)
-      return(item->chave);
-    exit(1);
+   if (item != NULL) return(item->chave);
+   exit(1);
 }
 
 
@@ -44,6 +43,5 @@ bool item_set_chave(ITEM *item, int chave){
 }
 
 void item_imprimir(ITEM *item){
-     if (item != NULL)
-        printf("\n-->item: %d\n", item->chave);
+   if (item != NULL) printf("\n-->item: %d\n", item->chave);
 }
